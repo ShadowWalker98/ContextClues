cdb553af61b05d3859380c2f306122a41103ba22
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-06-22 12:38:20 +0530

    trying to put them in different scopes

---
+++ app.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+    var names =  ["Satyajit", "Samuel", "Neha", "Ruchitha", "Nihash"];
+    var places = ["bathroom", "kitchen", "living room", "dining room", "veranda",
+                 "driveway", "study", "bedroom", "cellar", "attic"];
+    var weapons = ["teddy bear", "screw", "mouse", "ironing stand", "chair", "gown", "pillow", "book", "battery",
+                   "dustbin", "charger","knife", "belt","sofa", "taser","remote","mobile", "shoe", "socks", "bolt"];
+
+    //console.log("hello");
+    var item = document.getElementById("bodytag");
+    console.log(item);
+    runLoop(item, names)
+
+
+    
+}
+
+function runLoop(item, names) {
+    for( var i = 0; i < 5; i++ ){
+        
+        function storeVal() {
+            return i;
+        }
+        
+        createHeader(item, i, storeVal, names);  
+    }
+}
+
+function createHeader(item, num, getI, names) {
+    var text = document.createTextNode("Accusation " + (num + 1));
+    var node = document.createElement("h3");
+    node.appendChild(text);
+    addListener(node, getI, names);
+    item.appendChild(node);
+}
+
+function addListener(ele, getI, names) {
+    ele.addEventListener("click", function() {
+         console.log(names[getI()])
+    });
+
+}
