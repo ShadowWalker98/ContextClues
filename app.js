591c60ef2eb7a84672fa2a0af12da4ea7539a15f
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-06-22 13:13:30 +0530

    done

--- app.js
+++ app.js
@@ -8,34 +8,33 @@ window.onload = function() {
     //console.log("hello");
     var item = document.getElementById("bodytag");
     console.log(item);
-    runLoop(item, names)
+    runLoop(item, names, weapons, places);
 
 
     
 }
 
-function runLoop(item, names) {
-    for( var i = 0; i < 5; i++ ){
+function runLoop(item, names, weapons, places) {
+    for( var i = 0; i < 20; i++ ){
         
         function storeVal() {
             return i;
         }
         
-        createHeader(item, i, storeVal, names);  
+        createHeader(item, i, names, weapons, places);  
     }
 }
 
-function createHeader(item, num, getI, names) {
+function createHeader(item, num, names, weapons, places) {
     var text = document.createTextNode("Accusation " + (num + 1));
     var node = document.createElement("h3");
     node.appendChild(text);
-    addListener(node, num, names);
+    addListener(node, num, names, weapons, places);
     item.appendChild(node);
 }
 
-function addListener(ele, num, names) {
+function addListener(ele, num, names, weapons, places) {
     ele.addEventListener("click", function() {
-         console.log(names[num])
+         console.log("Accusation " + (num+1) + ": I accuse " + names[num%(names.length)] + ", with the " + weapons[num%(weapons.length)]," in the ", places[num%(places.length)] + "!");
     });
-
 }
